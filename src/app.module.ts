371ef4e10e8d5040ec3a7cb2a9347e9aebc4c4aa
Leonan Carvalho <j.leonancarvalho@gmail.com>
2025-06-22 14:51:09 -0300

    chore: create initial project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MatchesModule } from './matches/matches.module';
+import { PlayersModule } from './players/players.module';
+import { ParserModule } from './parser/parser.module';
 
 @Module({
-  imports: [],
+  imports: [MatchesModule, PlayersModule, ParserModule],
   controllers: [AppController],
   providers: [AppService],
 })
