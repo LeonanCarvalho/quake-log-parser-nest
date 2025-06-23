dd48059c8fc820a154e1fa69276e48d68737d703
Leonan Carvalho <j.leonancarvalho@gmail.com>
2025-06-22 22:37:55 -0300

    doc: add open api decorators

--- src/ranking/ranking.controller.ts
+++ src/ranking/ranking.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { RankingService } from './ranking.service';
 import { GlobalRankingPlayerDto } from './dto/global-ranking-response.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('ranking')
 @Controller('ranking')
 export class RankingController {
   constructor(private readonly rankingService: RankingService) {}
 
   @Get('global')
+  @ApiOperation({ summary: 'Busca o ranking global de jogadores agregando todas as partidas.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Ranking global retornado com sucesso.',
+    type: [GlobalRankingPlayerDto],
+  })
   async getGlobalRanking(): Promise<GlobalRankingPlayerDto[]> {
     return this.rankingService.getGlobalRanking();
   }
